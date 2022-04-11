7d6f7067136eb75ebe960e480e6f69e96ffb0d2f
Gabriel Amorim <gabriel._amorim@hotmail.com>
2022-04-11 10:29:49 -0300

    inicializando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Tela404 from './components/404/Index';
+import TelaInicial from './components/TelaInicial/Index';
+import GlitchTexto from './components/GlitchTexto/Index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TelaInicial/>
+      <Tela404/>
+      <GlitchTexto/>
     </div>
   );
 }
